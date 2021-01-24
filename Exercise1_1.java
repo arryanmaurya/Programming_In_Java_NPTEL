8c5aa5f307751a311738a383f05962dbdc7ebd9b
arryanmaurya <71206464+arryanmaurya@users.noreply.github.com>
2021-01-24 21:30:19 +0530

    Update Exercise1_1.java

--- Exercise1_1.java
+++ Exercise1_1.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;  
+      import java.util.Scanner;  
 public class Exercise1_1 {
        public static void main(String[] args) {
 Scanner s = new Scanner(System.in); 
        double radius= s.nextDouble();
        double perimeter;
        double area;
-//Calculate the perimeter 
-
-	perimeter = 2 * Math.PI * radius;
-//Calculate the area
-	area = perimeter * radius;
-	
-	System.out.println(perimeter);
-	System.out.println(area);
-	}
-}
+	double pi=Math.PI;
+       if(radius<=0)
+       {
+         System.out.print(" please enter non zero positive number");
+       }
+	   else
+       {
+         	perimeter=2*pi*radius;
+         	area=pi*radius*radius;
+         	System.out.print(perimeter+"\n"+area);
+       }
